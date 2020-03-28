adbacb3a82e2cd8dd7d9c7736db0b66135cb80fc
Gabriel-A-Rocha <gabriel.altair.rocha@gmail.com>
2020-03-27 23:19:14 -0300

    Front-End do App

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes';
+
+
+function App() {
+    return (
+    <Routes />
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
